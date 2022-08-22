54519aded9f0b98b4227bbcad3506ffbc659b172
Marcelo Daniel <marcelodaniel.daniel@gmail.com>
2022-08-22 17:17:57 -0300

    initial configs

---
+++ gateway/src/main/java/br/com/springbootbrasil/gateway/config/security/SecurityConfiguration.java
@@ -0,0 +1,24 @@
+package br.com.springbootbrasil.gateway.config.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.method.configuration.EnableReactiveMethodSecurity;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@EnableWebFluxSecurity
+@EnableReactiveMethodSecurity
+public class SecurityConfiguration {
+    @Bean
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
+        http
+                .authorizeExchange()
+                //ALLOWING REGISTER API FOR DIRECT ACCESS
+                .pathMatchers("/**").permitAll()
+                //ALL OTHER APIS ARE AUTHENTICATED
+                .anyExchange().authenticated()
+                .and()
+                .csrf().disable();
+        return http.build();
+    }
+}
--- service-registry/src/main/java/br/com/springbootbrasil/serviceregistry/ServiceRegistryApplication.java
+++ service-registry/src/main/java/br/com/springbootbrasil/serviceregistry/ServiceRegistryApplication.java
@@ -2,8 +2,10 @@ package br.com.springbootbrasil.serviceregistry;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class ServiceRegistryApplication {
     public static void main(String[] args) {
         SpringApplication.run(ServiceRegistryApplication.class, args);
