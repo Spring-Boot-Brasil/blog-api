fd6116c44cf7db64c295493b77cf9115b85f1190
Marcelo Daniel <marcelodaniel.daniel@gmail.com>
2022-10-25 22:06:10 -0300

    trabalhando nos casos de uso

--- domain/src/main/java/br/com/springbootbrasil/domain/service/UserService.java
+++ domain/src/main/java/br/com/springbootbrasil/domain/service/UserService.java
@@ -1,20 +1,27 @@
-package br.com.springbootbrasil.domain.usecases;
+package br.com.springbootbrasil.domain.service;
 
 import br.com.springbootbrasil.domain.dto.request.UserLoginRequest;
 import br.com.springbootbrasil.domain.dto.response.UserLoginResponse;
+import br.com.springbootbrasil.domain.dto.response.UserRegisterResponse;
+import br.com.springbootbrasil.domain.model.User;
 import br.com.springbootbrasil.domain.repository.UserRepository;
 import org.springframework.stereotype.Service;
 
 @Service
-public class UserLoginUseCase {
+public class UserService {
 
     private final UserRepository userRepository;
 
-    public UserLoginUseCase(UserRepository userRepository) {
+    public UserService(UserRepository userRepository) {
         this.userRepository = userRepository;
     }
 
     public UserLoginResponse auth(UserLoginRequest userLoginRequest) {
+        // TODO implementação
+        return null;
+    }
+
+    public UserRegisterResponse register(User entity) {
         // TODO implementação
         return null;
     }
